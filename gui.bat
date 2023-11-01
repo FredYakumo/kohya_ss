28ed48f868f75485b7fad6081548300d0b6cf4c6
fredyakumo <fredyakumo@gmail.com>
2023-11-01 12:49:00 +0800

    Modify run shell

--- gui.bat
+++ gui.bat
@@ -1,26 +1,9 @@
 @echo off
 
-:: Deactivate the virtual environment
-call .\venv\Scripts\deactivate.bat
+set http_proxy=http://127.0.0.1:7890
+set https_proxy=http://127.0.0.1:7890
 
-:: Calling external python program to check for local modules
-python .\setup\check_local_modules.py --no_question
+git config http.proxy http://127.0.0.1:7890 --replace-all
+git config https.proxy http://127.0.0.1:7890 --replace-all
 
-:: Activate the virtual environment
-call .\venv\Scripts\activate.bat
-set PATH=%PATH%;%~dp0venv\Lib\site-packages\torch\lib
-
-:: Validate requirements
-python.exe .\setup\validate_requirements.py
-
-:: If the exit code is 0, run the kohya_gui.py script with the command-line arguments
-if %errorlevel% equ 0 (
-    REM Check if the batch was started via double-click
-    IF /i "%comspec% /c %~0 " equ "%cmdcmdline:"=%" (
-        REM echo This script was started by double clicking.
-        cmd /k python.exe kohya_gui.py %*
-    ) ELSE (
-        REM echo This script was started from a command prompt.
-        python.exe kohya_gui.py %*
-    )
-)
+python.exe kohya_gui.py --server_port 7861 --headless %*
