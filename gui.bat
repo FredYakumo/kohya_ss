1a183c246695396416d88cd4fe524d25e9538c3b
fredyakumo <fredyakumo@gmail.com>
2023-11-03 10:09:28 +0800

    update run config

--- gui.bat
+++ gui.bat
@@ -3,7 +3,6 @@
 set http_proxy=http://127.0.0.1:7890
 set https_proxy=http://127.0.0.1:7890
 
-git config http.proxy http://127.0.0.1:7890 --replace-all
-git config https.proxy http://127.0.0.1:7890 --replace-all
+call C:\ProgramData\anaconda3\Scripts\activate C:\Users\fredyakumo\.conda\envs\kohya_ss
 
-python.exe kohya_gui.py --server_port 7861 --headless %*
+python.exe kohya_gui.py --server_port 7861
